0a79ad66376902acf5d72d5d7b21f38da6c54caf
Ayush Thapa <thapaaayush115@gmail.com>
2024-02-18 23:30:18 +0545

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+import createPWAConfig from 'next-pwa';
+
+const withPWA = createPWAConfig({
+  dest: 'public',
+  cacheOnFrontEndNav: true,
+ 
+  reloadOnOnline: true,
+
+  // disable: process.env.NODE_ENV === 'development',
+
+  // ... other options you like
+});
+
+const config = {
+  
+};
+
+export default process.env.NEXT_PUBLIC_ENVIRONMENT === 'DEV' ? withPWA(config) : withPWA(config);
